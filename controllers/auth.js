e4d40f5f55ecd87c966e65e7384606f935f7b5eb
sayash chaudhary <sayashchaudhary@gmail.com>
2020-04-14 21:52:53 +0530

    [SIGNUP] Validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const User = require('../models/user');
 exports.signup = (req, res) => {
     const user = new User(req.body);
     user.save((err, user) => {
-        if(err) {
+        if (err) {
             return res.status(400).json({
                 err: 'Not able to save the user'
             })
