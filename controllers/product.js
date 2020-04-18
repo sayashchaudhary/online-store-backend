ce86d22c8938d88b1c3ba93c54382a191c7a36e0
sayash chaudhary <sayashchaudhary@gmail.com>
2020-04-18 23:42:40 +0530

    adding product to db

--- controllers/product.js
+++ controllers/product.js
@@ -1,4 +1,7 @@
 const Product = require('../models/product');
+const formidable = require('formidable');
+const _ = require('lodash');
+const fs = require('fs');
 
 exports.getProductById = (req, res, id, next) => {
     Product.findById(id)
@@ -14,3 +17,50 @@ exports.getProductById = (req, res, id, next) => {
         })
 };
 
+exports.createProduct = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, file) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'problem with image'
+            })
+        }
+        //destructure the field
+        const { name, description, price, category, stock } = fields;
+        if (
+            !name ||
+            !description ||
+            !price ||
+            !category ||
+            !stock
+        ) {
+            return res.status(400).json({
+                error: 'please include all fields'
+            })
+        }
+
+        let product = new Product(fields);
+
+        //handling file
+        if (file.photo) {
+            if (file.photo.size > 3000000) {
+                return res.status(400).json({
+                    error: 'file size too big'
+                })
+            }
+            product.photo.data = fs.readFileSync(file.photo.path);
+            product.photo.data.contentType = file.photo.type
+        }
+
+        //save to db
+        product.save((err, product) => {
+            if (err) {
+                return res.json({
+                    error: 'saving t-shirt in db failes'
+                })
+            }
+            res.json(product)
+        })
+    })
+};
--- routes/product.js
+++ routes/product.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const { isSignedIn, isAuthenticated, isAdmin } = require('../controllers/auth');
 const { getUserById } = require('../controllers/user');
-const { getProductById } = require('../controllers/product');
+const { getProductById, createProduct } = require('../controllers/product');
 
 //all of params
 router.param('userId', getUserById);
 router.param('productId', getProductById);
 
+//actual routes
+router.post('/product/create/:userId', isSignedIn, isAuthenticated, isAdmin, createProduct);
