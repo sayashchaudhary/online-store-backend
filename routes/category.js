bb80ff7392467e57ed38e61b786ac91bb2d2a3f2
sayash chaudhary <sayashchaudhary@gmail.com>
2020-04-18 16:05:50 +0530

    creating a category

--- routes/category.js
+++ routes/category.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const { getCategoryById } = require('../controllers/category');
+const { getCategoryById, createCategory } = require('../controllers/category');
 const { isSignedIn, isAdmin, isAuthenticated } = require('../controllers/auth');
 const { getUserById } = require('../controllers/user');
 
+// Params
 router.param('userId', getUserById);
 router.param('categoryId', getCategoryById);
+
+// Actual Routers
+router.post('/category/create/:userId', isSignedIn, isAuthenticated, isAdmin, createCategory);
 
 module.exports = router;
 
