a5f1041d215c3c0aa0da6cd68ac4ebcf31e62fdf
sayash chaudhary <sayashchaudhary@gmail.com>
2020-04-20 20:15:42 +0530

    [Order] controller, route

---
+++ routes/order.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const { isSignedIn, isAuthenticated, isAdmin } = require('../controllers/auth');
+const { getUserById, pushOrderInPurchaseList } = require('../controllers/user');
+const { updateStock } = require('../controllers/product');
+const { getOrderById, createOrder } = require('../controllers/order');
+
+//params
+router.param('userId', getUserById);
+router.param('orderId', getOrderById);
+
+//actual routes
+
+//create
+router.post(
+    '/order/create/:userId',
+    isSignedIn,
+    isAuthenticated,
+    pushOrderInPurchaseList,
+    updateStock,
+    createOrder);
+
+module.exports = router;
