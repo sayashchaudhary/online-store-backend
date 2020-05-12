3b3c016ed0c95ca2a585187f70408030663a7d2b
sayash chaudhary <sayashchaudhary@gmail.com>
2020-05-12 15:48:06 +0530

    payment gateway

---
+++ controllers/stripepayment.js
@@ -0,0 +1,30 @@
+const stripe = require('stripe')('SECRET_KEY');
+const uuid = require('uuid/v4');
+
+exports.makePayment = (req, res) => {
+    const { products, token } = req.body;
+    console.log('PRODUCTS', products);
+    let amount = 0;
+    products.map(p => {
+        return amount = amount + p.price
+    });
+
+    const idempotencyKey = uuid();
+    return stripe.customers.create({
+        email: token.email,
+        source: token.id
+    })
+        .then(customer => {
+            stripe.charges.create({
+                amount: amount,
+                currency: 'usd',
+                customer: customer.id,
+                receipt_email: token.email,
+                shipping: {
+                    name: token.card.name
+                }
+            }, { idempotencyKey })
+                .then(result => res.status(200).json(result))
+                .catch(err => console.log(err))
+        })
+};
